8debb65c5dbbcfabc2882e41d61c6f043dafa70e
Steven Hyland <2knowindeed@gmail.com>
2021-04-13 19:02:57 -0400

    add react-bootstrap and customHeaders.yml. we'll see if bootstrap sticks...

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
-import { useUser} from '../util/auth';
+import { useUser } from '../util/auth';
+
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export function AuthComponent() {
   const { user, login, logout, completeNewPassword } = useUser();
@@ -8,64 +12,56 @@ export function AuthComponent() {
   const [newPassword, setNewPassword] = useState('');
   const [email, setEmail] = useState('');
 
-  console.log(user);
-
   // signed in, but this is the first time and a pw update/email is needed
   if (user && user.challengeName === 'NEW_PASSWORD_REQUIRED') {
     return (
-      <div>
-        <div>
-          You need to update your email and pick a new password.
-        </div>
-        <label>
-          Email:
-          <input type="text"
-            value={email}
-            onChange={e => setEmail(e.target.value)} />
-        </label>
-        <label>
-          New Password:
-          <input type="password"
-            value={newPassword}
-            onChange={e => setNewPassword(e.target.value)} />
-        </label>
-        <button onClick={() => {
-          completeNewPassword(email, newPassword);
-        }}
-        >Update</button>
-      </div>
+      <Form>
+        <Form.Text className="text-muted">You need to update your email and pick a new password.</Form.Text>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+          </Form.Group>
+          <Form.Group as={Col} controlId="formNewPassword">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
+          </Form.Group>
+          <Button variant="primary" type="button" onClick={() => {
+            completeNewPassword(email, newPassword);
+          }}>Update</Button>
+        </Form.Row>
+      </Form>
     );
   } 
   
   // signed in, all good
   if (user && !user.challengeName) {
     return (
-      <div>
-        <div>You are signed in as {user.username}.</div>
-        <button onClick={logout}>Sign out</button>
-      </div>
+      <Form>
+        <Form.Group as={Col} controlId="formSignedIn">
+          <Form.Text className="text-muted">You are signed in as {user.username}.</Form.Text>
+          <Button variant="primary" type="button" onClick={logout}>Sign out</Button>
+        </Form.Group>
+      </Form>
     );
   }
   
   // not signed in
   return (
-    <div>
-      <label>
-        Username: 
-        <input type="text" 
-          value={username} 
-          onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password: 
-        <input type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button onClick={() => {
-        login(username, password);
-      }}
-      >Sign in</button>
-    </div>
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="button" onClick={() => {
+          login(username, password);
+        }}>Sign in</Button>
+      </Form.Row>
+    </Form>
   );
 }
