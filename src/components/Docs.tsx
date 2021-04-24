ee9c12e90258b4b6c15e4075d738366b1dbeb581
Steven Hyland <2knowindeed@gmail.com>
2021-04-24 12:27:51 -0400

    add some bucket policies for unauth'd users, deny all public access to bucket

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -1,4 +1,8 @@
+import Auth from "@aws-amplify/auth";
+import Button from "react-bootstrap/Button";
 import { Link, useParams } from "react-router-dom"
+import { API, Storage } from 'aws-amplify';
+import { listTodos } from '../graphql/queries';
 
 
  function Docs() {
@@ -8,6 +12,15 @@ import { Link, useParams } from "react-router-dom"
     <div>
       <h3>ID: { id }</h3>
       <Link to="/">Back home</Link>
+      <Button variant="primary" type="button" onClick={async () => {
+        var creds = await Auth.currentCredentials();
+        console.log(creds);
+        const data: any = await API.graphql({ query: listTodos });
+        console.log(data);
+        Storage.put("testing1234", "testing1234", { level: 'public', customPrefix: { public: 'uploads/' }})
+          .then(result => console.log(result))
+          .catch(err => console.log(err));
+      }}>Test Auth</Button>
     </div>
   );
 }
