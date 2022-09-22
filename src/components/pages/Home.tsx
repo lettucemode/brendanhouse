63feb06bd24cce51a2f3e7d09d537f57e81b7aa0
Steven Hyland <2knowindeed@gmail.com>
2022-09-21 21:15:35 -0400

    latest changes, updates, and tweaks

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,7 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
-import Contact from '../Contact';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+import Contact from "../Contact";
 
 function Home() {
   return (
@@ -11,32 +11,34 @@ function Home() {
         <Row>
           {/* text and such */}
           <Row className="mt-4 mx-3">
-            <h1 className="text-bold text-center">A house of spiritual renewal for makers &amp; artists...</h1>
+            <h1 className="text-bold text-center">
+              A house of spiritual renewal for makers &amp; artists...
+            </h1>
           </Row>
           <Row className="mt-2 mx-4">
             <p>
-              …devoted to joyful, utter abandonment to Jesus Christ through prayerful use of the graces of making and
-              the arts. Rooted first in worship, we seek to pass on what we’ve been given by becoming deployable
-              creative resources for the kingdom of God in the areas of healing and discipleship. We grow into this
-              capacity in ourselves through four means:
+              …devoted to joyful, utter abandonment to Jesus Christ through prayerful use of the graces of
+              making and the arts. Rooted first in worship, we seek to pass on what we’ve been given by
+              becoming deployable creative resources for the kingdom of God in the areas of healing and
+              discipleship. We grow into this capacity in ourselves through four means:
             </p>
           </Row>
           <div className="mx-5">
-            <Link style={{ marginLeft: '40px' }} to="/prayer">
+            <Link style={{ marginLeft: "40px" }} to="/prayer">
               Corporate Prayer &amp; Worship
             </Link>
             <br />
-            <Link style={{ marginLeft: '40px' }} to="/training">
+            <Link style={{ marginLeft: "40px" }} to="/training">
               Training
             </Link>
             , including <Link to="/vessels-of-honor">Vessels of Honor</Link>
             , a spiritual direction training program in the Anglican Tradition
             <br />
-            <Link style={{ marginLeft: '40px' }} to="/community">
+            <Link style={{ marginLeft: "40px" }} to="/community">
               Affectionate Community
             </Link>
             <br />
-            <Link style={{ marginLeft: '40px' }} to="/creativity">
+            <Link style={{ marginLeft: "40px" }} to="/creativity">
               Thoughtful Creative Expression
             </Link>
           </div>
@@ -44,9 +46,9 @@ function Home() {
 
         <Row className="mt-4 mx-3">
           <p>
-            Brendan House is led by the Rev. Canon Joanne Martin with a team of like-minded adventurers. For more
-            information about Brendan House or the Vessels of Honor training program, please fill out the contact form
-            below.
+            Brendan House is led by Canon Joanne Martin with a team of like-minded adventurers. For more
+            information about Brendan House or the Vessels of Honor training program, please fill out the
+            contact form below.
           </p>
         </Row>
 
@@ -63,20 +65,23 @@ function Home() {
         <p className="sidebar-heading">Why Brendan?</p>
         <div className="sidebar-text">
           <p>
-            Brendan was a real person who lived in Ireland in the late 400's ce. He was a monk, best known for setting
-            out on a sea voyage to find the Promised Land...even though he didn't know where it was! Most ancient
-            pilgrims set out with a specific destination in mind. Brendan went in response to a vision. His story, and
-            that of the small community that accompanied him, is told in an ancient text called
+            Brendan was a real person who lived in Ireland in the late 400's ce. He was a monk, best known for
+            setting out on a sea voyage to find the Promised Land...even though he didn't know where it was!
+            Most ancient pilgrims set out with a specific destination in mind. Brendan went in response to a
+            vision. His story, and that of the small community that accompanied him, is told in an ancient
+            text called
             <i> The Voyage of St. Brendan</i>. He is the model for Brendan House because:
           </p>
           <ul>
-            <li>Of his obedience to a vision, responding to an urge to go even when he didn't know the way</li>
+            <li>
+              Of his obedience to a vision, responding to an urge to go even when he didn't know the way
+            </li>
             <li>His trust in God that allowed him to take great risks</li>
             <li>His openness to joy and adventure</li>
             <li>The faithful patterns of worship kept by him and the small community that accompanied him</li>
             <li>
-              Of the way he was changed by his voyage, and how he returned to share what he'd seen with the people he'd
-              left behind
+              Of the way he was changed by his voyage, and how he returned to share what he'd seen with the
+              people he'd left behind
             </li>
           </ul>
         </div>
--- src/components/pages/VesselsOfHonor.tsx
+++ src/components/pages/VesselsOfHonor.tsx
@@ -1,16 +1,18 @@
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Popover from 'react-bootstrap/Popover';
-import PopoverTitle from 'react-bootstrap/PopoverTitle';
-import PopoverContent from 'react-bootstrap/PopoverContent';
-import { useHistory } from 'react-router-dom';
-import { FileService, S3File } from '../../services/file';
-import { useEffect, useMemo, useState } from 'react';
-import useWindowDimensions from '../../util/useWindowDimenstions';
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import Popover from "react-bootstrap/Popover";
+import PopoverTitle from "react-bootstrap/PopoverTitle";
+import PopoverContent from "react-bootstrap/PopoverContent";
+import Image from "react-bootstrap/Image";
+import { useHistory } from "react-router-dom";
+import { FileService, S3File } from "../../services/file";
+import { useEffect, useMemo, useState } from "react";
+import useWindowDimensions from "../../util/useWindowDimenstions";
+import jarsImage from "../../img/jars-image.jpg";
 
 function VesselsOfHonor() {
   const history = useHistory();
@@ -36,16 +38,18 @@ function VesselsOfHonor() {
             <h2 className="text-bold text-center">Vessels of Honor Spiritual Direction Training Program</h2>
           </Col>
         </Row>
-        <div className="voh-background">
+        <div>
           <p>
-            Vessels of Honor is a spiritual direction training program in the Anglican tradition, dedicated to raising
-            up spiritual companions and directors equipped for kingdom service. The curriculum design is an interweaving
-            of three strands: personal formation (emphasizing grounding in God’s presence), skill building, and a
-            foundational knowledge base for the ministry of spiritual companioning/direction. The community context
-            created by the weekend intensives is essential to the program. In other words, this training is not about
-            “head knowledge,” or “conceptual teaching.” It is about forming who the participant is, in preparation to
-            serve in a particular way in the wider ministry of the church.
+            Vessels of Honor is a spiritual direction training program in the Anglican tradition, dedicated to
+            raising up spiritual companions and directors equipped for kingdom service. The curriculum design
+            is an interweaving of three strands: personal formation (emphasizing grounding in God’s presence),
+            skill building, and a foundational knowledge base for the ministry of spiritual
+            companioning/direction. The community context created by the weekend intensives is essential to
+            the program. In other words, this training is not about “head knowledge,” or “conceptual
+            teaching.” It is about forming who the participant is, in preparation to serve in a particular way
+            in the wider ministry of the church.
           </p>
+          <Image src={jarsImage} rounded className="jarsfloated" />
           <p>The cohort structure and curriculum have been built on the foundation of five core values:</p>
           <ul>
             <li>Trinitarian worship: Father, Son and Holy Spirit</li>
@@ -62,10 +66,10 @@ function VesselsOfHonor() {
             <li>Is established/participating regularly in a worshipping community</li>
           </ol>
           <p>
-            Applicants who are interested in developing their skills in spiritual companioning, but are unsure about
-            vocational ministry as a spiritual director, are invited to apply for Year One. Applicants who are
-            interested in or feeling called to the vocational ministry of spiritual direction are strongly encouraged to
-            apply for both years.
+            Applicants who are interested in developing their skills in spiritual companioning, but are unsure
+            about vocational ministry as a spiritual director, are invited to apply for Year One. Applicants
+            who are interested in or feeling called to the vocational ministry of spiritual direction are
+            strongly encouraged to apply for both years.
           </p>
         </div>
 
@@ -73,17 +77,17 @@ function VesselsOfHonor() {
         <Row>
           <Col md={1} lg={1} xl={2} />
           <Col>
-            <Card style={{ width: '100%' }}>
+            <Card style={{ width: "100%" }}>
               <Card.Body>
                 <Card.Title>Cohort Resources</Card.Title>
                 <Row className="mx-auto">
                   <Col>
-                    <Button variant="primary" onClick={() => history.push('/docs')}>
+                    <Button variant="primary" onClick={() => history.push("/docs")}>
                       Cohort Resources
                     </Button>
                   </Col>
                   <Col>
-                    <Button variant="secondary" onClick={() => history.push('/payments')}>
+                    <Button variant="secondary" onClick={() => history.push("/payments")}>
                       Make a Payment
                     </Button>
                   </Col>
@@ -102,85 +106,104 @@ function VesselsOfHonor() {
           <ul>
             <li>
               <a
-                href={siteFiles.find((sf: S3File) => sf.s3Obj.key === 'site/voh_overview.pdf')?.signedUrl}
+                href={siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_overview.pdf")?.signedUrl}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Overview of the Program
+                Program Overview
               </a>
             </li>
           </ul>
         </Row>
         <Row className="mt-3 mb-2">
-          <OverlayTrigger
-            key="curroverview"
-            placement="top"
-            overlay={<Tooltip id={`tooltip-curroverview`}>Coming soon!</Tooltip>}
-          >
+          <ul>
+            <li>Curriculum Overview</li>
             <ul>
-              <li>Curriculum Overview</li>
+              <li>
+                <a
+                  href={
+                    siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_curr_over_y1.pdf")?.signedUrl
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Year One
+                </a>
+              </li>
+              <OverlayTrigger
+                key="curroverview"
+                placement={width > 992 ? "left" : "top"}
+                overlay={<Tooltip id={`tooltip-curroverview`}>Coming soon!</Tooltip>}
+              >
+                <li>Year Two</li>
+              </OverlayTrigger>
             </ul>
-          </OverlayTrigger>
+          </ul>
         </Row>
         <Row>
-          <OverlayTrigger
-            show={showAppPopup}
-            placement={width > 992 ? 'left' : 'top'}
-            onExit={() => setShowAppPopup(false)}
-            overlay={
-              <Popover id="popover-app-process">
-                <PopoverTitle as="h3">Application Process</PopoverTitle>
-                <PopoverContent>
-                  <div onMouseLeave={() => setShowAppPopup(!showAppPopup)}>
-                    <p>
-                      The application cycle is closed until 2022. Generally applications are accepted March-June for a
-                      cohort starting in the Fall. If there is sufficient interest, cohorts may also start at other
-                      times of the year.
-                    </p>
-                    <p>
-                      Once an application is received, there is an interview with the cohort leader. All applicants will
-                      be fully considered, though priority consideration will be given to male applicants because of the
-                      urgent need in our region at this time.
-                    </p>
-                    <ul>
-                      <li>
-                        <a
-                          href={siteFiles.find((sf: S3File) => sf.s3Obj.key === 'site/voh_app_form.pdf')?.signedUrl}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Sample Application
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href={siteFiles.find((sf: S3File) => sf.s3Obj.key === 'site/voh_ref_form.pdf')?.signedUrl}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Sample Reference Form
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            }
-          >
-            <ul>
-              <li onClick={() => setShowAppPopup(!showAppPopup)}>
-                <Button style={{ marginLeft: '-12px' }} variant="link" onClick={(e) => e.preventDefault()}>
-                  Application Process
-                </Button>
-              </li>
-            </ul>
-          </OverlayTrigger>
+          <div onMouseLeave={() => setShowAppPopup(false)}>
+            <OverlayTrigger
+              show={showAppPopup}
+              placement={width > 992 ? "left" : "top"}
+              onExit={() => setShowAppPopup(false)}
+              overlay={
+                <Popover id="popover-app-process">
+                  <PopoverTitle as="h3">Application Process</PopoverTitle>
+                  <PopoverContent>
+                    <div>
+                      <p>
+                        Applications for <i>Vessels of Honor</i> are accepted on a rolling admission basis.
+                        The next Year 1 Cohort is set to begin in early 2023. Location TBD, but likely to be
+                        South Carolina.
+                      </p>
+                      <ul>
+                        <li>
+                          <a
+                            href={
+                              siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_app_form.pdf")
+                                ?.signedUrl
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Application Form
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href={
+                              siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_ref_form.pdf")
+                                ?.signedUrl
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            References Copy and Form
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              }
+            >
+              <ul>
+                <li onClick={() => setShowAppPopup(!showAppPopup)}>
+                  <Button style={{ marginLeft: "-12px" }} variant="link" onClick={(e) => e.preventDefault()}>
+                    Application Process
+                  </Button>
+                </li>
+              </ul>
+            </OverlayTrigger>
+          </div>
         </Row>
         <Row className="mt-2 mb-3">
           <ul>
             <li>
               <a
-                href={siteFiles.find((sf: S3File) => sf.s3Obj.key === 'site/voh_financial_info.pdf')?.signedUrl}
+                href={
+                  siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_financial_info.pdf")?.signedUrl
+                }
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -193,7 +216,7 @@ function VesselsOfHonor() {
           <ul>
             <li>
               <a
-                href={siteFiles.find((sf: S3File) => sf.s3Obj.key === 'site/voh_booklist.pdf')?.signedUrl}
+                href={siteFiles.find((sf: S3File) => sf.s3Obj.key === "site/voh_booklist.pdf")?.signedUrl}
                 target="_blank"
                 rel="noopener noreferrer"
               >
